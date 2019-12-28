7902574cf770b6350b615adad9c6e4c030fbf721
jyooj08 skkusosc <jyooj08@naver.com>
2019-12-28 17:36:12 +0900

    Solve #15596

---
+++ Step6/15596.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+long long sum(std::vector<int> &a) {
+	long long ans = 0;
+    int size = a.size();
+    
+    for(int i=0;i<size;i++){
+    	ans += a.at(i);
+	}
+	return ans;
+}
+
