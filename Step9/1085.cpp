ce516bcaa540bea41c7e3bf951d97218d8aa3cb5
jyooj08 skkusosc <jyooj08@naver.com>
2019-12-31 12:13:42 +0900

    Solve #1085

---
+++ Step9/1085.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int x, y, w, h;
+	cin >>x>>y>>w>>h;
+	
+	int min=x;
+	if(min>y) min=y;
+	if(min>w-x) min=w-x;
+	if(min>h-y) min=h-y;
+	
+	cout << min;
+	
+	return 0;
+}
