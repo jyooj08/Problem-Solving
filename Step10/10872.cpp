c681dcb18ecf82e72807fc04a44f42e3effb7f41
jyooj08 skkusosc <jyooj08@naver.com>
2020-01-01 20:01:29 +0900

    Solve #10872

---
+++ Step10/10872.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+	if(n==1) return 1;
+	else return n * factorial(n-1);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	if(n==0) cout << 1;
+	else cout << factorial(n);
+	
+	return 0;
+}
