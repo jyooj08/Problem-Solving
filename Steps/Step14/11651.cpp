9af7dcc3573191f0288984f4ee450aeceeead51a
Yujin Jang <jyooj08@gmail.com>
2020-01-19 21:07:12 +0900

    Solve #11651

---
+++ Steps/Step14/11651.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+class Point{
+public:
+	int x;
+	int y;
+};
+
+bool increase(Point a, Point b){
+	if(a.y < b.y) return true;
+	else if (a.y == b.y){
+		if(a.x < b.x) return true;
+		else return false;
+	} else return false;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	Point points[n];
+	
+	for(int i=0;i<n;i++) cin >> points[i].x >> points[i].y;
+	
+	sort(points, points+n, increase);
+	
+	for(int i=0;i<n;i++) cout << points[i].x << ' ' << points[i].y << '\n';
+	
+	return 0;
+}
