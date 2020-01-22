ea2f748464b10606ab957d965ebb0cef9c24af01
Yujin Jang <jyooj08@gmail.com>
2020-01-22 17:18:36 +0900

    Solve #15650

---
+++ Steps/Step15/15650.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void search(vector<int> v, int n, int m, int k){
+	if(v.size()==0 || k > v.back()) v.push_back(k);
+	else return;
+	
+	if(v.size()==m){
+		for(int i=0;i<m;i++) cout << v[i] << ' '; cout << '\n';
+		return;
+	}
+	
+	for(int i=1;i<=n;i++){
+		search(v,n,m,i);
+	}
+}
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	
+	vector<int> v;
+
+	for(int i=1;i<=n;i++) {
+		search(v, n, m, i);	
+	}
+	
+	return 0;
+}
