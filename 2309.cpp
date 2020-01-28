b75dd0bc1783312ebbf385faf0c40863e0aa85fc
Yujin Jang <jyooj08@gmail.com>
2020-01-28 11:45:06 +0900

    Solve #2309 in another way

--- 2309.cpp
+++ 2309.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main()
 {
 	int height[9];
-	bool checked[9]={0,};
+	bool checked[9]; fill_n(checked, 9, true);
 
 	for(int i=0;i<9;i++) cin >> height[i];
 	sort(height, height+9);
 	
+	/*
 	bool find=false;
 	for(int i=0;i<8;i++){
 		checked[i]=true;
@@ -30,5 +31,21 @@ int main()
 	}
 	
 	for(int i=0;i<9;i++) if(!checked[i]) cout << height[i] << '\n';
+	*/
+	
+	int sum = 0;
+	for(int i=0;i<9;i++) sum += height[i];
+	
+	for(int i=0;i<8;i++){
+		for(int j=i+1;j<9;j++){
+			if(sum - height[i] - height[j] == 100){
+				checked[i]=false;
+				checked[j]=false;
+				
+				for(int k=0;k<9;k++) if(checked[k]) cout << height[k] << '\n';
+				return 0;
+			}
+		}
+	}
 	return 0;
 }
