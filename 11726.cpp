0162d8843fa3a5591098e805d9dc1ae6c4207d72
Yujin Jang <jyooj08@gmail.com>
2020-02-01 14:01:22 +0900

    Solve #11726

---
+++ 11726.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	int arr[n+1]={0,};
+	
+	arr[1]=1;
+	arr[2]=2;
+	for(int i=3;i<=n;i++){
+		arr[i] = (arr[i-1] + arr[i-2])%10007;
+	}
+	
+	cout << arr[n];
+	
+	return 0;
+}
