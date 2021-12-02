575c306955ddcc4d84b5def80b0c7c81ddb7db69
Yujin Jang <jyooj08@gmail.com>
2021-12-02 15:38:04 +0900

    good

---
+++ programmers/두_개_뽑아서_더하기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    int len = numbers.size();
+    
+    for(int i=0;i<len-1;i++){
+        for(int j=i+1; j<len; j++){
+            answer.push_back(numbers[i]+numbers[j]);
+        }
+    }
+    
+    sort(answer.begin(), answer.end());
+    answer.erase(unique(answer.begin(),answer.end()),answer.end());
+    return answer;
+}
